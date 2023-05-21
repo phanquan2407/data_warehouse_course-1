27fe3320695e66495c291d15855120ae2d55048a
vannguyen <vannguyen123ccd@gmail.com>
2023-05-21 17:22:59 +0700

    he

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,6 +1,6 @@
 SELECT 
-  stock_item_id product_key, 
-  stock_item_name product_name,
-  brand brand_name
+  cast(stock_item_id as int) product_key, 
+  cast(stock_item_name as string) product_name,
+  cast(brand as string) brand_name
 FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
  
