924613e553c1713af21fc9f9f4592ab36f64eb74
vannguyen <vannguyen123ccd@gmail.com>
2023-05-21 17:07:04 +0700

    change key

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,6 +1,7 @@
-SELECT 
+SELECT
   order_line_id sale_order_line_key,
   quantity,
   unit_price,
-  quantity*unit_price gross_amount
+  quantity*unit_price gross_amount,
+  stock_item_id product_key
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
