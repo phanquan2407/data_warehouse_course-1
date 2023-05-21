0e890a92688e0db1cb9ea22f8ca2ec059a6a80c1
vannguyen <vannguyen123ccd@gmail.com>
2023-05-21 17:26:17 +0700

    a

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,7 @@
 SELECT
-  order_line_id sale_order_line_key,
-  quantity,
-  unit_price,
-  quantity*unit_price gross_amount,
-  stock_item_id product_key
+  cast(order_line_id as int) sale_order_line_key,
+  cast(quantity as int) quantity,
+  cast(unit_price as numeric) unit_price,
+  cast(quantity as int) *cast(unit_price as numeric) gross_amount,
+  cast(stock_item_id as int) product_key
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
