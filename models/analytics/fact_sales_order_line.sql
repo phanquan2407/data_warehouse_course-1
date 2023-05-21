88ec34d3227e4659824b8c07b6b6fc8aa87da592
vannguyen <vannguyen123ccd@gmail.com>
2023-05-21 16:40:01 +0700

    chamge

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,5 +1,6 @@
 SELECT 
   order_line_id sale_order_line_key,
   quantity,
-  unit_price
+  unit_price,
+  quantity*unit_price gross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
