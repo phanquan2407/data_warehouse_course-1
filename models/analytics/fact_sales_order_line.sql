fa25049e93ec367ca7c0f186acfe67f72b4857e7
vannguyen <vannguyen123ccd@gmail.com>
2023-05-23 21:46:52 +0700

    s

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -3,5 +3,7 @@ SELECT
   cast(quantity as int) quantity,
   cast(unit_price as numeric) unit_price,
   cast(quantity as int) *cast(unit_price as numeric) gross_amount,
-  cast(stock_item_id as int) product_key
-FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+  cast(stock_item_id as int) product_key,
+  cast(so.customer_id as int) customer_key
+FROM `vit-lam-data.wide_world_importers.sales__order_lines` sol 
+LEFT JOIN `vit-lam-data.wide_world_importers.sales__orders` so on cast(ol.order_id as int) = cast(so.order_id as int)
