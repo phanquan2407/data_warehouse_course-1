bf34868a98ff92ac26a90714d01a52a0c8fe375b
vannguyen <vannguyen123ccd@gmail.com>
2023-05-23 21:32:39 +0700

    s

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,4 +1,4 @@
 SELECT 
-  customer_id customer_key,
-  customer_name 
+  cast(customer_id as int) customer_key,
+  cast(customer_name as string) customer_name 
 FROM `vit-lam-data.wide_world_importers.sales__customers`
