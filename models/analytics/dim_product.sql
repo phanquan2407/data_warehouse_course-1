82cd7c803d269e13fdbf7e71d64b7a52de40b1e3
vannguyen <vannguyen123ccd@gmail.com>
2023-05-25 22:20:49 +0700

    s

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -3,7 +3,7 @@ SELECT
   cast(stock_item_name as string) product_name,
   cast(brand as string) brand_name,
   cast(ps.supplier_id as int) supplier_key,
-  cast(ps.supplier_name as string)
+  cast(ps.supplier_name as string) supplier_name
 FROM `vit-lam-data.wide_world_importers.warehouse__stock_items` wst 
 left join `vit-lam-data.wide_world_importers.purchasing__suppliers` ps on cast(wst.supplier_id as int) = cast(ps.supplier_id as int)
  
